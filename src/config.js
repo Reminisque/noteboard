cf9f6f178e1537df87469fe24ba7a998dfafc292
Travis Chang <travisc2728@gmail.com>
2020-02-07 00:27:57 -0800

    Added Firebase config template file

---
+++ src/config.js
@@ -0,0 +1,13 @@
+var config = {
+  'firebase': {
+    apiKey: "__API_KEY__",
+    authDomain: "__AUTH_DOMAIN__",
+    databaseURL: "__DATABASE_URL__",
+    projectId: "__PROJECT_ID__",
+    storageBucket: "__STORAGE_BUCKET__",
+    messagingSenderId: "__MESSAGING_SENDER_ID__",
+    appId: "__FIREBASE_APP_ID__"
+  }
+}
+
+export default config;
