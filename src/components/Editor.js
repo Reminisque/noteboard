57a4587d3466658a3adee49207cd64679c9b3801
Reminisque <travisc2728@gmail.com>
2020-02-11 02:13:51 -0800

    Styling and render for editor and notelist
    Coded up the render functions for editor and notelist components.
    Also added some functions to update note information for both components.

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,21 +1,48 @@
 import React from 'react';
+import ReactQuill from 'react-quill';
+import debounce from '../helpers';
 import './Editor.css';
 
 class Editor extends React.Component {
   constructor() {
     super();
     this.state = {
-        
-    }
+      id: '',
+      title: '',
+      body: '',
+    };
   }
 
   render() {
-    return (<div>This is where you edit notes</div>);
+    return (
+      <div className="editor-container">
+        <input
+          type="text"
+          className="note-title-input"
+          placeholder="Untitled"
+          onChange={(e) => this.updateTitle(e.target.value)}>
+        </input>
+        <ReactQuill
+          value={this.state.body}
+          onChange={this.updateBody}>
+        </ReactQuill>
+      </div>
+    );
   }
-  
-  componentDidMount() {
-      
+
+  updateTitle = async (val) => {
+    await this.setState({ title: val });
+    this.update();
   }
+
+  updateBody = async (val) => {
+    await this.setState({ body: val });
+    this.update();
+  };
+
+  update = debounce(() => {
+    console.log("Updating database: \n", this.state);
+  }, 1500);
 }
 
 export default Editor;
--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -2,8 +2,52 @@ import React from 'react';
 import './NoteList.css';
 
 class NoteList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      addingNote: false,
+      title: ''
+    };
+  }
+
   render() {
-    return (<div>All the notes will be here</div>)
+    const { notes, selectedNoteIndex } = this.props;
+    return (
+      <div className="notelist-container">
+        <button 
+          className="new-note-btn"
+          onClick={this.toggleAddingNote}>
+          { this.state.addingNote ? "Cancel" : "New Note" }
+        </button>
+        {
+          this.state.addingNote ? 
+            <div>
+              <input type="text"
+                className="new-note-input"
+                placeholder="Enter note title"
+                onChange={(e) => this.updateTitle(e.target.value)}>
+              </input>
+              <button
+                className="new-note-submit-btn"
+                onClick={this.submitNote}>
+                Submit Note
+              </button>
+            </div> : null
+        }
+      </div>
+    );
+  }
+
+  updateTitle = (val) => {
+    this.setState({ title: val });
+  };
+
+  toggleAddingNote = () => {
+    this.setState({ title: null, addingNote: !this.state.addingNote });
+  }
+
+  submitNote = () => {
+    console.log("Submitted note: ", this.state.title);
   }
 }
 
